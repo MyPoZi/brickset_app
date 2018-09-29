9985d623639a88cbd912cdc2878ab55c5cc91b22
MyPoZi <kxx555u.u@gmail.com>
2018-09-30 03:51:24 +0900

    add signup function for enhancement #3

--- brickset_app/accounts/urls.py
+++ brickset_app/accounts/urls.py
@@ -1,8 +1,5 @@
-from django.conf.urls import url
-from django.contrib.auth.urls import views as auth_views
 from django.urls import path
-from django.views.generic import TemplateView
-from django.contrib.auth.decorators import user_passes_test
+
 
 from . import views
 
@@ -12,4 +9,6 @@ urlpatterns = [
     path('', views.Top.as_view(), name='top'),
     path('login/', views.Login.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('create/', views.create, name='create'),
 ]
--- brickset_app/accounts/views.py
+++ brickset_app/accounts/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (
     LoginView, LogoutView
 )
+from django.http import HttpResponseRedirect, Http404
 from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
 from .forms import LoginForm
 # Create your views here.
@@ -23,3 +25,19 @@ class Top(generic.TemplateView):
 class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'accounts/top.html'
+
+
+def signup(request):
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('./login')
+        return render(request, 'accounts/signup.html', {'form': form,})
+    else:
+        raise Http404
